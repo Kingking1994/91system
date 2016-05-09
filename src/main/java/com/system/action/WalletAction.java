fb25030ea8277db8d30280ddbffa5857d6e1e115
king <929966345@qq.com>
2016-05-09 15:38:25 +0800

    钱包相关动作，显示交易记录或者退款记录，充值

---
+++ src/main/java/com/system/action/WalletAction.java
@@ -0,0 +1,119 @@
+package com.system.action;
+
+import com.system.entity.UserInfo;
+import com.system.entity.Wallet;
+import com.system.enums.UserIdentifiedEnum;
+import com.system.service.RefundService;
+import com.system.service.TradeService;
+import com.system.service.UserInfoService;
+import com.system.service.WalletService;
+import com.system.util.StrUtil;
+import org.apache.log4j.Logger;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Actions;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by king on 2016/5/6.
+ */
+@Namespace("/wallets")
+public class WalletAction extends SuperAction {
+
+    private int rechargeAmount;//充值的金额
+
+    private static final Logger LOGGER = Logger.getLogger(WalletAction.class);
+
+    @Autowired
+    private UserInfoService userInfoService;
+
+    @Autowired
+    private WalletService walletService;
+
+
+
+    /**
+     * 钱包
+     * @return
+     */
+    @Actions({
+            //交易记录
+            @Action(value = "trade",results = {
+                    @Result(name = "success",location = "../success.jsp"),
+                    @Result(name = "failure",location = "../failure.jsp")
+            }),
+            //退款记录
+            @Action(value = "refund",results = {
+                    @Result(name = "success",location = "../success.jsp"),
+                    @Result(name = "failure",location = "../failure.jsp")
+            })
+    })
+    public String wallet(){
+        try {
+            if(StrUtil.isNotBlank((String) session.getAttribute("userPhone"))){
+                if((Integer)session.getAttribute("identify") == UserIdentifiedEnum.NO.index){
+                    LOGGER.info("该用户还没有完善个人信息");
+                    return "failure";
+                }else{
+                    UserInfo u = userInfoService.findByPhone((String) session.getAttribute("userPhone"));
+                    Wallet wallet = u.getWallet();
+                    session.setAttribute("wallet",wallet);
+                    return "success";
+                }
+            }else{
+                LOGGER.warn("用户没有登录");
+                session.setAttribute("errorMsg","用户没有登录");
+                return "failure";
+            }
+        }catch (Exception e){
+            LOGGER.error(e);
+            return "failure";
+        }
+    }
+
+
+    /**
+     * 钱包充值
+     * @return
+     */
+    @Action(value = "recharge",results = {
+            @Result(name = "success",location = "../success.jsp"),
+            @Result(name = "failure",location = "../failure.jsp")
+    })
+    public String recharge(){
+        try {
+            if(StrUtil.isNotBlank((String) session.getAttribute("userPhone"))){
+                if((Integer)session.getAttribute("identify") == UserIdentifiedEnum.NO.index){
+                    LOGGER.info("该用户还没有完善个人信息");
+                    return "failure";
+                }else{
+                    UserInfo u = userInfoService.findByPhone((String) session.getAttribute("userPhone"));
+                    Wallet wallet = u.getWallet();
+                    wallet.setAccount(wallet.getAccount() + rechargeAmount);
+                    walletService.saveOrUpdate(wallet);
+                    session.setAttribute("wallet",wallet);
+                    LOGGER.info("recharge : " + rechargeAmount);
+                    return "success";
+                }
+            }else{
+                LOGGER.warn("用户没有登录");
+                session.setAttribute("errorMsg","用户没有登录");
+                return "failure";
+            }
+        }catch (Exception e){
+            LOGGER.error(e);
+            return "failure";
+        }
+    }
+
+
+
+    public int getRechargeAmount() {
+        return rechargeAmount;
+    }
+
+    public void setRechargeAmount(int rechargeAmount) {
+        this.rechargeAmount = rechargeAmount;
+    }
+}
