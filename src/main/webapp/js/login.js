6144c0210507cd62c39038dc4a5784596e339f48
king <929966345@qq.com>
2016-05-23 20:48:15 +0800

    解除页面显示隐藏绑定，只剩下充值dialog

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -2,9 +2,9 @@
  * Created by xiaohang on 2016/5/4.
  */
 $(document).ready(function(){
-   findcontent();
-   detailshow();
-   settingchange();
+   //findcontent();
+   //detailshow();
+   //settingchange();
    dialogshow();
 });
 function change(obj){
@@ -23,58 +23,58 @@ function change(obj){
         $(".tit_span").text("用户注册");
     }
 }
-function settingchange(){
-   $(".account_list").delegate('li', 'click', function() {
-
-      $(this).addClass('current');
-      $(this).siblings().removeClass('current');
-      if($(this).text()=="账号信息"||$(this).text()=="交易记录")
-      {
-         $(".lan_left").show();
-          $(".lan_right").hide();
-      }
-      else
-      {
-         $(".lan_right").show();
-          $(".lan_left").hide();
-      }
-   });
-}
-function findcontent(){
-    $(".user_menu").delegate('.user_f', 'click', function() {
-      // $(this) .addClass('bg_on');
-        $(".order").filter(":visible").hide();
-      if($(this).text()=="订单管理")
-        { 
-          $(".right_title").text($(this).text());
-          $(".right_content .order_list").show();
-      
-        }
-        else{
-          if($(this).text()=="账号设置")
-        { 
-          $(".right_title").text($(this).text());
-          $(".right_content .account").show();
-        }
-        else{
-          $(".right_title").text($(this).text());
-          $(".right_content .col_main").show();
-        }
-
-        }
-    });
-}
-function detailshow(){
-    $(".order_list").delegate('.right_item', 'click', function() {
-    $(".order_detail").show();
-    $(".right_content .order_list").hide();
-  });
-    $(".order_detail .title a").bind('click',  function() {
-    $(".order_detail").hide();
-    $(".right_content .order_list").show();
-  /* Act on the event */
-});
-}
+//function settingchange(){
+//   $(".account_list").delegate('li', 'click', function() {
+//
+//      $(this).addClass('current');
+//      $(this).siblings().removeClass('current');
+//      if($(this).text()=="账号信息"||$(this).text()=="交易记录")
+//      {
+//         $(".lan_left").show();
+//          $(".lan_right").hide();
+//      }
+//      else
+//      {
+//         $(".lan_right").show();
+//          $(".lan_left").hide();
+//      }
+//   });
+//}
+//function findcontent(){
+//    $(".user_menu").delegate('.user_f', 'click', function() {
+//      // $(this) .addClass('bg_on');
+//        $(".order").filter(":visible").hide();
+//      if($(this).text()=="订单管理")
+//        {
+//          $(".right_title").text($(this).text());
+//          $(".right_content .order_list").show();
+//
+//        }
+//        else{
+//          if($(this).text()=="账号设置")
+//        {
+//          $(".right_title").text($(this).text());
+//          $(".right_content .account").show();
+//        }
+//        else{
+//          $(".right_title").text($(this).text());
+//          $(".right_content .col_main").show();
+//        }
+//
+//        }
+//    });
+//}
+//function detailshow(){
+//    $(".order_list").delegate('.right_item', 'click', function() {
+//    $(".order_detail").show();
+//    $(".right_content .order_list").hide();
+//  });
+//    $(".order_detail .title a").bind('click',  function() {
+//    $(".order_detail").hide();
+//    $(".right_content .order_list").show();
+//  /* Act on the event */
+//});
+//}
 function dialogshow(){
   $(".recharge_btn").bind('click',  function() {
      $(".recharge_dialog").show();
