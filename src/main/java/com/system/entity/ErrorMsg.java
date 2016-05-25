efb808e5177f806f4c30869d0c8ecb5e1f1cdf0d
king <929966345@qq.com>
2016-05-25 23:33:33 +0800

    错误信息类

---
+++ src/main/java/com/system/entity/ErrorMsg.java
@@ -0,0 +1,44 @@
+package com.system.entity;
+
+/**
+ * 错误信息类
+ * Created by king on 2016/5/25.
+ */
+public class ErrorMsg {
+
+    public int code;
+
+    public String message;
+
+    public ErrorMsg(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public ErrorMsg() {
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "ErrorMsg{" +
+                "code=" + code +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
